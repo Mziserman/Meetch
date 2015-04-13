a2f69dac77c0bb49f758054d52bc912f8a7c1ff2
Mziserman <ziserman@MacBook-Pro-de-Ziserman.local>
2015-04-13 16:53:53 +0200

    put name and id in english

--- inscription.php
+++ inscription.php
@@ -14,11 +14,11 @@
 </head>
 <body>
 	<form action="#" method="post">
-		<input type="text" name="nom" id="nom">
-		<label for="nom">Nom</label>
+		<input type="text" name="name" id="name">
+		<label for="name">Nom</label>
 		<br/>
-		<input type="text" name="prenom">
-		<label for="prenom" id="prenom">Prénom</label>
+		<input type="text" name="surname">
+		<label for="surname" id="surname">Prénom</label>
 		 <p>
 		    <input type="date" name="birth">
 	   		<label for="birth">Date de naissance</label>
@@ -37,16 +37,16 @@
 	    </p>
 	    <p>
 	    	<label>Vous recherchez :<br/>
-				<input type="checkbox" name="smen" id="smen">
-				<label for="smen">Un homme</label>
+				<input type="checkbox" name="smale" id="smale">
+				<label for="smale">Un homme</label>
 				<input type="checkbox" name="sfemale" id="sfemale">
 				<label for="sfemale">Une femme</label>
 			</label>
 		</p>
 	    <p>
-   	   		<label>Que recherchez-vous ? <br/>
-				<input type="checkbox" name="sasoeur" id="sasoeur">
-				<label for="sasoeur">Ame soeur</label>
+   	   		<label>Vous recherchez : <br/>
+				<input type="checkbox" name="soulmate" id="soulmate">
+				<label for="soulmate">Ame soeur</label>
 				<br/>
 				<input type="checkbox" name="snight" id="snight">
 				<label for="snight">Série d'un soir</label>
@@ -56,12 +56,14 @@
 				<br/>
 			</label>
 		</p>
-		<select name="age" id="sage">
-			<option value="0">18 à 29 ans</option>
-			<option value="1">30 à 39 ans</option>
-			<option value="2">40 à 49 ans</option>
-			<option value="3">50 ans et plus</option>
-		</select>
+		<label>Dans quelle tranche d'age : <br/>
+			<select name="age" id="sage">
+				<option value="0">18 à 29 ans</option>
+				<option value="1">30 à 39 ans</option>
+				<option value="2">40 à 49 ans</option>
+				<option value="3">50 ans et plus</option>
+			</select>
+		</label>
 		<br/>
 		<input type="email" name="mail" id="mail">
 		<label for="mail">Votre email</label>
